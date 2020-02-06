2bd0ba3f9243f380cb0b6761076c4e6bc26852d6
ozcan-codification <ozcan.ozkaya@codification.io>
2020-02-06 11:06:40 +0000

    Reorganising the repo

--- Scheduling/7_Static_pods/create-static-obselete-pod.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl run --restart=Never --image=busybox static-obselete-pod --dry-run -o yaml --command -- sleep 1000 > /etc/kubernetes/manifests/static-obselete-pod.yaml
-
-ssh root@node01 mkdir /etc/kubernetes/manifests
-
-scp /etc/kubernetes/manifests/static-obselete-pod.yaml root@node01:/etc/kubernetes/manifests/
